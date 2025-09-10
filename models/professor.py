a907e33cc4a1c45331e006ac146f67a0332263ee
eric silva <ericmiguelbdasilva.com@gmail.com>
2025-09-10 20:43:03 -0300

    Adicionando estrutura e primeiros arquivos db

---
+++ models/professor.py
@@ -0,0 +1,13 @@
+from models import db
+
+class Professor(db.Model):
+    __tablename__ = 'professor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
+    materia = db.Column(db.String(100), nullable=False)
+    observacoes = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f"<Professor {self.nome}>"
