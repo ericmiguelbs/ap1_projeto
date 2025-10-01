57e4eefa68c8e57caf4f24f0fdcd61b38ed3a207
eric silva <ericmiguelbdasilva.com@gmail.com>
2025-09-30 22:10:14 -0300

    Corrigindo rotas aluno e professor

--- app.py
+++ app.py
@@ -21,22 +21,22 @@ with app.app_context():
 
 app.add_url_rule('/lista_aluno', view_func=AlunoController.listar,methods = ['GET'],endpoint='listar_alunos')
 
-app.add_url_rule('/atualiza_aluno', view_func=AlunoController.atualizar,methods = ['PUT'],endpoint='atualiza_aluno')
+app.add_url_rule('/atualiza_aluno/<int:id>', view_func=AlunoController.atualizar,methods = ['PUT'],endpoint='atualiza_aluno')
 
-app.add_url_rule('/deleta_aluno', view_func=AlunoController.deletar,methods = ['DELETE'],endpoint='deleta_aluno')
+app.add_url_rule('/deleta_aluno/<int:id>', view_func=AlunoController.deletar,methods = ['DELETE'],endpoint='deleta_aluno')
 
 app.add_url_rule('/criar_aluno', view_func=AlunoController.criar,methods = ['POST'],endpoint='criar_alunos')
 
 app.add_url_rule('/lista_professor', view_func = ProfessorController.listar,methods = ['GET'],endpoint = 'listar_professores')
 
 app.add_url_rule('/adiciona_professor', view_func = ProfessorController.criar,methods = ['POST'],endpoint = 'adiciona_professores')
-app.add_url_rule('/deleta_professor', view_func = ProfessorController.deletar,methods = ['DELETE'],endpoint = 'deleta_professores')
-app.add_url_rule('/atualiza_professor', view_func = ProfessorController.atualizar,methods = ['PUT'],endpoint = 'atualiza_professores')
+app.add_url_rule('/deleta_professor/<int:id>', view_func = ProfessorController.deletar,methods = ['DELETE'],endpoint = 'deleta_professores')
+app.add_url_rule('/atualiza_professor/<int:id>', view_func = ProfessorController.atualizar,methods = ['PUT'],endpoint = 'atualiza_professores')
 
 app.add_url_rule('/lista_turmas', view_func=TurmaController.listar, methods=['GET'], endpoint='lista_turmas')
 app.add_url_rule('/cria_turmas', view_func=TurmaController.criar, methods=['POST'], endpoint='cria_turmas')
 app.add_url_rule('/atualiza_turmas/<int:id>', view_func=TurmaController.atualizar, methods=['PUT'], endpoint='atualiza_turmas')
-app.add_url_rule('/tdeleta_turmas/<int:id>', view_func=TurmaController.deletar, methods=['DELETE'], endpoint='deleta_turmas')
+app.add_url_rule('/deleta_turmas/<int:id>', view_func=TurmaController.deletar, methods=['DELETE'], endpoint='deleta_turmas')
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug = True)
